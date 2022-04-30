e61cd6141ebd5717366790535a5f01cd8c33cdaf
doston <dos400dos400@gmail.com>
2022-04-30 19:11:37 +0500

    add content name and image

--- app/src/main/java/uz/hamroev/linuxgo/fragment/HomeFragment.kt
+++ app/src/main/java/uz/hamroev/linuxgo/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import uz.hamroev.linuxgo.R
 import uz.hamroev.linuxgo.adapters.ModulAdapter
 import uz.hamroev.linuxgo.adapters.SubjectAdapter
@@ -13,28 +14,8 @@ import uz.hamroev.linuxgo.models.Content
 import uz.hamroev.linuxgo.models.Modul
 import uz.hamroev.linuxgo.models.Subject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     lateinit var binding: FragmentHomeBinding
     lateinit var list: ArrayList<Subject>
@@ -59,8 +40,6 @@ class HomeFragment : Fragment() {
 
 
 
-
-
         return binding.root
     }
 
@@ -70,7 +49,64 @@ class HomeFragment : Fragment() {
             listModule,
             object : ModulAdapter.OnModuleItemCLickListener {
                 override fun onCLick(modul: Modul, position: Int, contentPosition: Int) {
-
+                    when (position) {
+                        0 -> {
+                            when (contentPosition) {
+                                0 -> {
+                                }
+                                1 -> {
+                                }
+                                2 -> {
+                                }
+                                3 -> {
+                                }
+                                4 -> {
+                                }
+                                5 -> {
+                                }
+                                6 -> {
+                                }
+                                7 -> {
+                                }
+                            }
+                        }
+                        1 -> {
+                            when (contentPosition) {
+                                0 -> {
+                                }
+                                1 -> {
+                                }
+                                2 -> {
+                                }
+                                3 -> {
+                                }
+                                4 -> {
+                                }
+                                5 -> {
+                                }
+                                6 -> {
+                                }
+                            }
+                        }
+                        2 -> {
+                            when (contentPosition) {
+                                0 -> {
+                                }
+                                1 -> {
+                                }
+                                2 -> {
+                                }
+                                3 -> {
+                                }
+                                4 -> {
+                                }
+                                5 -> {
+                                }
+                                6 -> {
+                                }
+                            }
+                        }
+                    }
                 }
             })
         binding.rvModule.adapter = modulAdapter
@@ -80,22 +116,186 @@ class HomeFragment : Fragment() {
         listModule = ArrayList()
 
         val listContent1 = ArrayList<Content>()
-        listContent1.add(Content("", "", "8 Lesson", R.drawable.ic_getting_started))
-        listContent1.add(Content("", "", "8 Lesson", R.drawable.ic_getting_started))
-        listContent1.add(Content("", "", "8 Lesson", R.drawable.ic_getting_started))
-        listContent1.add(Content("", "", "8 Lesson", R.drawable.ic_getting_started))
+        listContent1.add(
+            Content(
+                resources.getString(R.string.getting_started),
+                resources.getString(R.string.getting_started_info),
+                resources.getString(R.string.lesson_count_getting_started),
+                R.drawable.ic_getting_started
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.command_line),
+                resources.getString(R.string.command_line_info),
+                resources.getString(R.string.lesson_count_command_line),
+                R.drawable.command_line
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.text_fu),
+                resources.getString(R.string.text_fu_info),
+                resources.getString(R.string.lesson_count_text_fu),
+                R.drawable.text_fu
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.text_fu_advanced),
+                resources.getString(R.string.text_fu_advanced_info),
+                resources.getString(R.string.lesson_count_text_fu_advanced),
+                R.drawable.text_fu_advanced
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.user_management),
+                resources.getString(R.string.user_management_info),
+                resources.getString(R.string.lesson_count_user_management),
+                R.drawable.user_management
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.permissions),
+                resources.getString(R.string.permissions_info),
+                resources.getString(R.string.lesson_count_permissions),
+                R.drawable.access
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.processes),
+                resources.getString(R.string.processes_info),
+                resources.getString(R.string.lesson_count_processes),
+                R.drawable.processes
+            )
+        )
+        listContent1.add(
+            Content(
+                resources.getString(R.string.packages),
+                resources.getString(R.string.packages_info),
+                resources.getString(R.string.lesson_count_packages),
+                R.drawable.package_management
+            )
+        )
 
         val listContent2 = ArrayList<Content>()
-        listContent2.add(Content("", "", "6 Lesson", R.drawable.ic_getting_started))
-        listContent2.add(Content("", "", "6 Lesson", R.drawable.ic_getting_started))
-        listContent2.add(Content("", "", "6 Lesson", R.drawable.ic_getting_started))
-        listContent2.add(Content("", "", "6 Lesson", R.drawable.ic_getting_started))
+        listContent2.add(
+            Content(
+                resources.getString(R.string.devices),
+                resources.getString(R.string.devices_info),
+                resources.getString(R.string.lesson_count_devices),
+                R.drawable.devices
+            )
+        )
+        listContent2.add(
+            Content(
+                resources.getString(R.string.the_filesystem),
+                resources.getString(R.string.the_filesystem_info),
+                resources.getString(R.string.lesson_count_the_filesystem),
+                R.drawable.filesystem
+            )
+        )
+        listContent2.add(
+            Content(
+                resources.getString(R.string.boot_the_system),
+                resources.getString(R.string.boot_the_system_info),
+                resources.getString(R.string.lesson_count_boot_the_system),
+                R.drawable.booting
+            )
+        )
+        listContent2.add(
+            Content(
+                resources.getString(R.string.kernel),
+                resources.getString(R.string.kernel_info),
+                resources.getString(R.string.lesson_count_kernel),
+                R.drawable.kernel
+            )
+        )
+        listContent2.add(
+            Content(
+                resources.getString(R.string.init),
+                resources.getString(R.string.init_info),
+                resources.getString(R.string.lesson_count_init),
+                R.drawable.init
+            )
+        )
+        listContent2.add(
+            Content(
+                resources.getString(R.string.process_utilization),
+                resources.getString(R.string.process_utilization_info),
+                resources.getString(R.string.lesson_count_process_utilization),
+                R.drawable.process_utilization
+            )
+        )
+        listContent2.add(
+            Content(
+                resources.getString(R.string.logging),
+                resources.getString(R.string.logging_info),
+                resources.getString(R.string.lesson_count_logging),
+                R.drawable.logging
+            )
+        )
 
         val listContent3 = ArrayList<Content>()
-        listContent3.add(Content("", "", "4 Lesson", R.drawable.ic_getting_started))
-        listContent3.add(Content("", "", "4 Lesson", R.drawable.ic_getting_started))
-        listContent3.add(Content("", "", "4 Lesson", R.drawable.ic_getting_started))
-        listContent3.add(Content("", "", "4 Lesson", R.drawable.ic_getting_started))
+        listContent3.add(
+            Content(
+                resources.getString(R.string.network_sharing),
+                resources.getString(R.string.network_sharing_info),
+                resources.getString(R.string.lesson_count_network_sharing),
+                R.drawable.network_sharing
+            )
+        )
+        listContent3.add(
+            Content(
+                resources.getString(R.string.network_basic),
+                resources.getString(R.string.network_basic_info),
+                resources.getString(R.string.lesson_count_network_basic),
+                R.drawable.network_fundamentals
+            )
+        )
+        listContent3.add(
+            Content(
+                resources.getString(R.string.subnetting),
+                resources.getString(R.string.subnetting_info),
+                resources.getString(R.string.lesson_count_subnetting),
+                R.drawable.subnetting
+            )
+        )
+        listContent3.add(
+            Content(
+                resources.getString(R.string.routing),
+                resources.getString(R.string.routing_info),
+                resources.getString(R.string.lesson_count_routing),
+                R.drawable.routing
+            )
+        )
+        listContent3.add(
+            Content(
+                resources.getString(R.string.network_config),
+                resources.getString(R.string.network_config_info),
+                resources.getString(R.string.lesson_count_network_config),
+                R.drawable.network_configuration
+            )
+        )
+        listContent3.add(
+            Content(
+                resources.getString(R.string.troubleshooting),
+                resources.getString(R.string.troubleshooting_info),
+                resources.getString(R.string.lesson_count_troubleshooting),
+                R.drawable.network_troubleshooting
+            )
+        )
+        listContent3.add(
+            Content(
+                resources.getString(R.string.dns),
+                resources.getString(R.string.dns_info),
+                resources.getString(R.string.lesson_count_dns),
+                R.drawable.dns
+            )
+        )
 
         listModule.add(Modul("Grasshopper", listContent1))
         listModule.add(Modul("Journeyman", listContent2))
@@ -114,99 +314,33 @@ class HomeFragment : Fragment() {
             })
         binding.rvSubject.adapter = subjectAdapter
     }
+
     private fun loadSubjects() {
         list = ArrayList()
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
-        list.add(
-            Subject(
-                resources.getString(R.string.getting_started),
-                R.drawable.ic_getting_started
-            )
-        )
+        list.add(Subject(resources.getString(R.string.getting_started), R.drawable.ic_getting_started))
+        list.add(Subject(resources.getString(R.string.command_line), R.drawable.command_line))
+        list.add(Subject(resources.getString(R.string.text_fu), R.drawable.text_fu))
+        list.add(Subject(resources.getString(R.string.text_fu_advanced), R.drawable.text_fu_advanced))
+        list.add(Subject(resources.getString(R.string.user_management), R.drawable.user_management))
+        list.add(Subject(resources.getString(R.string.permissions), R.drawable.access))
+        list.add(Subject(resources.getString(R.string.processes), R.drawable.processes))
+        list.add(Subject(resources.getString(R.string.packages), R.drawable.package_management))
+        list.add(Subject(resources.getString(R.string.devices), R.drawable.devices))
+        list.add(Subject(resources.getString(R.string.the_filesystem), R.drawable.filesystem))
+        list.add(Subject(resources.getString(R.string.boot_the_system), R.drawable.booting))
+        list.add(Subject(resources.getString(R.string.kernel), R.drawable.kernel))
+        list.add(Subject(resources.getString(R.string.init), R.drawable.init))
+        list.add(Subject(resources.getString(R.string.process_utilization), R.drawable.process_utilization))
+        list.add(Subject(resources.getString(R.string.logging), R.drawable.logging))
+        list.add(Subject(resources.getString(R.string.network_sharing), R.drawable.network_sharing))
+        list.add(Subject(resources.getString(R.string.network_basic), R.drawable.network_fundamentals))
+        list.add(Subject(resources.getString(R.string.subnetting), R.drawable.subnetting))
+        list.add(Subject(resources.getString(R.string.routing), R.drawable.routing))
+        list.add(Subject(resources.getString(R.string.network_config), R.drawable.network_configuration))
+        list.add(Subject(resources.getString(R.string.troubleshooting), R.drawable.network_troubleshooting))
+        list.add(Subject(resources.getString(R.string.dns), R.drawable.dns))
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
